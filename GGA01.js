2b52a1141a5b5691730c726af71715e23eb3569e
0k0a <79756500+0k0a@users.noreply.github.com>
2021-12-23 01:22:57 +0900

    Update GGA01.js

--- GGA01.js
+++ GGA01.js
@@ -6,10 +6,13 @@ else {
   var e_div = document.getElementById('div_gga_file');
   var e_target = document.body;
   if ((e_div == null) && (e_target != null)) {
+    e_target.style.maxWidth = window.innerWidth + "px";
+    e_target.style.minHeight = "0px";
+    
     var e_div = document.createElement('div');
     e_div.id = 'div_gga_file';
     e_div.setAttribute('style', 'z-index:999999;position:fixed;top:0;background-color:white');
-    e_div.innerHTML = `<input id="txt_clip" type="text" style="width: 320px;height: 100px">&emsp;<button id="btn_GetName">get name</button>&emsp;<button id="btn_clear">clear</button><br/><div id="img_dsp"></div>`;
+    e_div.innerHTML = `<input id="txt_clip" type="text" style="width: 600px;height: 50px">&emsp;<button id="btn_GetName">get name</button>&emsp;<button id="btn_clear">clear</button><br/><div id="img_dsp"></div>`;
     document.body.appendChild(e_div);
 
     function fnGetImageSize() {
@@ -19,6 +22,9 @@ else {
     }
     
     function fnGetImageFileName(url) {
+      var sTmp = txt_clip.value;
+      var aTmp = sTmp.split("=");
+      url = aTmp[0];
       if(!window.XMLHttpRequest) {
         alert("The function(window.XMLHttpRequest) is not defined.");
         return;
@@ -36,7 +42,7 @@ else {
                 if (aTmp[i].substr(0, 9) == "filename=") {
                   var aTmp1 = aTmp[i].split("=");
                   if (aTmp1.length == 2) {
-                    img_dsp.innerHTML = '<img id="img_gga" src="' + url + '" onload="fnGetImageSize()"><br/><input id="txt_url" type="text" style="width: 320px;" value=""><br/><input id="txt_name" type="text" style="width: 320px;" value="' + aTmp1[1] + '">';
+                    img_dsp.innerHTML = '<img id="img_gga" src="' + url + '" onload="fnGetImageSize()"><br/><input id="txt_url" type="text" style="width: 600px;"><br/><input id="txt_name" type="text" style="width: 600px;" value="' + aTmp1[1] + '">';
                   }
                 }
               }
